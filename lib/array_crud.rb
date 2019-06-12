a20ad1acdb76455371fec0abd67dae7465350f9d
Adrian Popan <adrian.popan@gmail.com>
2019-06-12 00:02:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -5,9 +5,9 @@ end
 def create_an_array
   ["1", "2", "3", "4"]
 end
-  array << "5"
+
 def add_element_to_end_of_array(array, element)
-  
+  array << "#{element}"
 end
 
 def add_element_to_start_of_array(array, element)
